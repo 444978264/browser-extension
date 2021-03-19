edeb28b0b1af5077b8d72fbd383aa5e177f822b5
cyf <dvpnet@dvpnet.io>
2021-03-19 11:09:00 +0800

    nothing

--- src/content.js
+++ src/content.js
@@ -1,1 +1,5 @@
 console.log('this is content.js')
+const titleEl = document.getElementsByTagName('title')
+if (titleEl.length) {
+  titleEl[0].innerText = 'from content js'
+}
--- src/views/app.jsx
+++ src/views/app.jsx
@@ -4,6 +4,18 @@ import './app.scss'
 
 const About = lazy(() => import('./About'))
 
+function change() {
+  console.log(chrome, 'chrome')
+  // window.open(
+  //   chrome.runtime.getURL('popup.html'),
+  //   'new window',
+  //   'width=420,height=230,resizable,scrollbars=yes,status=1'
+  // )
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(tabs, 'tabs')
+  })
+}
+
 export function App() {
   return (
     <Router>
@@ -14,6 +26,8 @@ export function App() {
           <Link to="/users">users</Link>
           <br />
           <Link to="/">home1111</Link>
+          <br />
+          <button onClick={change}>change</button>
         </div>
         <Switch>
           <Route path="/about">
